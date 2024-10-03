79b0641f5e9bfb9de2d4412809fd523a8658a527
Asish <apatro817@gmail.com>
2024-10-03 17:41:25 +0530

    update ecs-service.tf

--- ecs-service.tf
+++ ecs-service.tf
@@ -1,8 +1,9 @@
+# Define the ECS service
 resource "aws_ecs_service" "medusa_service" {
   name            = "medusa-service"
   cluster         = aws_ecs_cluster.medusa_cluster.id
   task_definition = aws_ecs_task_definition.medusa_task.arn
-  desired_count   = 1
+  desired_count   = 1  # Initial count will be 1
 
   network_configuration {
     subnets          = [aws_subnet.subnet_1.id, aws_subnet.subnet_2.id]
@@ -13,5 +14,50 @@ resource "aws_ecs_service" "medusa_service" {
   capacity_provider_strategy {
     capacity_provider = "FARGATE_SPOT"
     weight            = 1
+  }
+
+  # Enable service auto-scaling
+  deployment_minimum_healthy_percent = 50
+  deployment_maximum_percent         = 200
+}
+
+# ECS Auto Scaling target
+resource "aws_appautoscaling_target" "ecs_target" {
+  max_capacity       = 3  # Maximum tasks
+  min_capacity       = 1  # Minimum tasks
+  resource_id        = "service/${aws_ecs_cluster.medusa_cluster.name}/${aws_ecs_service.medusa_service.name}"
+  scalable_dimension = "ecs:service:DesiredCount"
+  service_namespace  = "ecs"
+}
+
+# Scaling policy based on CPU utilization
+resource "aws_appautoscaling_policy" "cpu_scaling_policy" {
+  name               = "cpu-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageCPUUtilization"
+    }
+    target_value = 50.0  # Desired average CPU utilization target (50%)
+  }
+}
+
+# Scaling policy based on Memory utilization (Optional)
+resource "aws_appautoscaling_policy" "memory_scaling_policy" {
+  name               = "memory-scaling-policy"
+  policy_type        = "TargetTrackingScaling"
+  resource_id        = aws_appautoscaling_target.ecs_target.resource_id
+  scalable_dimension = aws_appautoscaling_target.ecs_target.scalable_dimension
+  service_namespace  = aws_appautoscaling_target.ecs_target.service_namespace
+
+  target_tracking_scaling_policy_configuration {
+    predefined_metric_specification {
+      predefined_metric_type = "ECSServiceAverageMemoryUtilization"
+    }
+    target_value = 60.0  # Desired average memory utilization target (60%)
   }
 }
